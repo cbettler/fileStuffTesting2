38a8655f8e63f79b8439c13378dbc8aa6258b749
Carlee Bettler <carleebettler@Carlees-MacBook-Air.local>
2016-04-02 00:32:25 -0500

    Initial Commit

---
+++ fileStuffTesting2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fileStuffTesting2
+//
+//  Created by Carlee Bettler on 4/2/16.
+//  Copyright (c) 2016 Carlee Bettler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
